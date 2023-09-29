0b76dbc23f0c67d6b4ba148aaf28a54d2d175628
emipignata <emipignata@gmail.com>
2023-09-29 18:08:23 -0300

    primer commit

---
+++ app/src/main/java/com/example/eminav/Fr_data.kt
@@ -0,0 +1,28 @@
+package com.example.eminav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class Frdata : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.fragment_fr_data, container, false)
+        val button = view.findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_fr_data_to_fr_home)
+        }
+        return view
+    }
+
+}
